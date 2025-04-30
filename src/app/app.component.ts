f70e9ef47959e8311e149dc58bac82b360682b2c
Tibs47 <tibor.bicanic@gmail.com>
2025-05-01 00:46:00 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { environment } from '../environments/environment';
+import { createClient } from '@supabase/supabase-js';
+import { CommonModule } from '@angular/common';
+
+const supabase = createClient(environment.supabaseUrl, environment.supabaseKey);
 
 @Component({
+  standalone: true,
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'jobs-app';
+  artikli: any[] = [];
+
+  async ngOnInit() {
+    const { data, error } = await supabase
+      .from('ARTIKLI') // or your table name, e.g., 'products'
+      .select('*');
+    if (error) {
+      console.error('Greška pri dohvaćanju artikala:', error.message);
+    } else {
+      this.artikli = data;
+      console.log(this.artikli);
+    }
+  }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    supabaseUrl: 'https://scovzfufqjdyjgugpubu.supabase.co',
+    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNjb3Z6ZnVmcWpkeWpndWdwdWJ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYwNDYxNDMsImV4cCI6MjA2MTYyMjE0M30.5r7l1g4EgV3cegmchXCVlvMaW_TF--LlLi1m8DmluGE'
+};
