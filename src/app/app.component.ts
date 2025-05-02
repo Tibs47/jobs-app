f6e851d9dd7e0f392e36e9c0be0be0c4e8b14c62
Tibs47 <tibor.bicanic@gmail.com>
2025-05-02 20:12:39 +0200

    99%

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { environment } from '../environments/environment';
 import { createClient } from '@supabase/supabase-js';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import Swal from 'sweetalert2';
 
 const supabase = createClient(environment.supabaseUrl, environment.supabaseKey);
 
@@ -20,60 +21,70 @@ export class AppComponent {
       colDesc: 'Kategorija',
       colWidth: '200px',
       textAlign: '',
+      admin: false,
     },
     {
       colName: 'name',
       colDesc: 'Artikl',
       colWidth: '300px',
       textAlign: '',
+      admin: false,
     },
     {
       colName: 'description',
       colDesc: 'Opis',
       colWidth: '450px',
       textAlign: '',
+      admin: false,
     },
     {
       colName: 'active',
       colDesc: 'Aktivno',
       colWidth: '90px',
       textAlign: '',
+      admin: true,
     },
     {
       colName: 'price',
       colDesc: 'Cijena',
       colWidth: '90px',
       textAlign: 'right',
+      admin: false,
     },
     {
       colName: 'currency',
       colDesc: 'Valuta',
       colWidth: '90px',
       textAlign: '',
+      admin: false,
     },
     {
       colName: 'in_stock',
       colDesc: 'U skladištu',
       colWidth: '150px',
-      textAlign: '',
+      textAlign: 'right',
+      admin: true,
     },
     {
       colName: 'unit',
       colDesc: 'Mj. jedinica',
       colWidth: '150px',
       textAlign: '',
+      admin: false,
     },
     {
       colName: 'created_at',
       colDesc: 'Kreirano',
       colWidth: '250px',
       textAlign: '',
+      admin: true,
     },
     {
       colName: 'last_update',
       colDesc: 'Ažurirano',
       colWidth: '250px',
       textAlign: '',
+      admin: true,
     },
   ];
 
@@ -82,7 +93,11 @@ export class AppComponent {
   public sortCol = { colName: '', direction: false };
 
   public showFilter = false;
+  public filterApplied = false;
   public filterData: { [key: string]: any } = {};
+
+  public showNewRow = false;
+  public newRow: { [key: string]: any } = {};
 
   public tooltipPosition = { top: '0px', left: '0px' };
   public showTooltip = false;
@@ -90,18 +105,51 @@ export class AppComponent {
 
   async ngOnInit() {
     this.sortCol = { colName: '', direction: false };
-    const { data, error } = await supabase
+    this.getArticles();
+  }
+
+  public toggleRoles(role: string) {
+    if(role === 'K') {
+      this.showNewRow = false;
+      this.title = 'Korisnik';
+    } else if (role === 'A'){
+      this.title = 'Admin';
+    }
+  }
+
+  public async getArticles() {
+    let query = supabase
       .from('ARTIKLI')
       .select('*');
+
+    if(this.filterApplied) {
+      for (const key in this.filterData) {
+        const value = this.filterData[key];
+        if (value !== '' && value !== null && value !== undefined) {
+          query = query.ilike(key, `%${value}%`);
+        }
+      }
+    }
+
+    if(this.sortCol.colName !== '') {
+      query = query.order(this.sortCol.colName, { ascending: this.sortCol.direction });
+    }
+
+    const { data, error } = await query;
     if (error) {
-      console.error('Greška pri dohvaćanju artikala:', error.message);
+      Swal.fire({
+        icon: 'error',
+        title: 'Dogodila se greška',
+        text: error.message,
+        showConfirmButton: true
+      });
     } else {
       this.artikli = data;
       console.log(this.artikli);
     }
   }
 
-  public async sortArticles(column: string) {
+  public sortArticles(column: string) {
     if(this.sortCol.colName === column) {
       if(this.sortCol.direction === true) {
         this.sortCol.direction = false;
@@ -112,33 +160,77 @@ export class AppComponent {
       this.sortCol.colName = column;
       this.sortCol.direction = false;
     }
-    const { data, error } = await supabase
-    .from('ARTIKLI')
-    .select('*')
-    .order(this.sortCol.colName, { ascending: this.sortCol.direction });
-    if (error) {
-      console.error('Greška pri dohvaćanju artikala:', error.message);
-    } else {
-      this.artikli = data;
-      console.log(this.artikli);
-    }
-  }
-
-  public async removeArticle(id: number) {
-    console.log(id);
+    this.getArticles();
   }
 
   public filterArticles() {
-
+    this.filterApplied = true;
+    if(!this.showFilter) {
+      this.showFilter = true;
+    } else {
+      this.getArticles();
+    }
   }
 
   public clearFilter() {
+    this.filterApplied = false;
     for (const key in this.filterData) {
       if (this.filterData.hasOwnProperty(key)) {
         this.filterData[key] = '';
       }
     }
-    this.ngOnInit();
+    this.getArticles();
+  }
+
+  public removeArticle(id: number, name: string) {
+    Swal.fire({
+      title: 'Jeste li sigurni da želite obrisati artikl?',
+      html: name,
+      icon: 'warning',
+      showConfirmButton: true,
+      confirmButtonText: 'Obriši',
+      confirmButtonColor: 'var(--red-color)',
+      showCancelButton: true,
+      cancelButtonText: 'Odustani'
+    }).then((result) => {
+      if(result.isConfirmed) {
+        this.confirmRemove(id);
+      }
+    });
+  }
+
+  public async confirmRemove(id: number) {
+    const { error } = await supabase
+    .from('ARTIKLI')
+    .delete()
+    .eq('id', id);
+    this.getArticles();
+  }
+
+  public async addArticle() {
+    console.log(this.newRow);
+  
+    const { data, error } = await supabase
+    .from('ARTIKLI')
+    .insert([this.newRow])
+    .select();
+
+    if (!error) {
+      Swal.fire({
+        icon: 'success',
+        title: 'Artikl dodan!',
+        showConfirmButton: false,
+        timer: 2000
+      });
+    } else {
+      Swal.fire({
+        icon: 'error',
+        title: 'Dogodila se greška',
+        text: error.message,
+        showConfirmButton: true
+      });
+    }
+    this.getArticles();
   }
 
   //tooltips START
@@ -149,7 +241,7 @@ export class AppComponent {
     } else {
       if (event) {
         this.tooltipPosition.top = event?.clientY + 20 + 'px';
-        this.tooltipPosition.left = event?.clientX - 20 + 'px';
+        this.tooltipPosition.left = event?.clientX - 10 + 'px';
         if (event?.clientX - 20 < 0) {
           this.tooltipPosition.left = '0px';
         }
